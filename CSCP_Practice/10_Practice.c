d11c09e8608eab42b93bf6f5882f3bf1dc820519
cfdemirkiran <115482444+cfdemirkiran@users.noreply.github.com>
2023-12-29 16:45:14 +0300

    Update 10_Practice.c

--- CSCP_Practice/10_Practice.c
+++ CSCP_Practice/10_Practice.c
@@ -4,16 +4,16 @@ int g = 3;
 
 int f1(int x)
 {
-	return x * g++; // 9 , g=4 /// 30 * 6 = 180
+	return x * g++;
 }
 
 int f2(int x)
 {
-	return x * ++g; // 9 * (1+4) = 45 /// 6 * 5 = 30
+	return x * ++g;
 }
 
 
 int main(void)
 {
-	printf("%d\n", f2(f1(g))); // 45
-	printf("%d\n", f1(f2(g))); // 180
+	printf("%d\n", f2(f1(g)));
+	printf("%d\n", f1(f2(g)));

dd7082963fa6d2d5da2477c3c22514da89fe6832
cfdemirkiran <115482444+cfdemirkiran@users.noreply.github.com>
2023-12-29 16:43:38 +0300

    Update 03_Practice.c

--- CSCP_Practice/03_Practice.c
+++ CSCP_Practice/03_Practice.c
@@ -5,14 +5,14 @@ int main(void)
 	int x = 2, y, z;
 
 	x *= 3 + 2;
-	printf("%d\n", x); //10
+	printf("%d\n", x);
 
 	x *= y = z = 4;
-	printf("%d\n", x); //40
+	printf("%d\n", x);
 
 	x = y == z;
-	printf("%d\n", x); //1
+	printf("%d\n", x);
 
 	x == (y = z);
-	printf("%d\n", x); //1
+	printf("%d\n", x);
 }

4cedf86f87c417d6841ee30dae4c5a14e9a5bbde
cfdemirkiran <115482444+cfdemirkiran@users.noreply.github.com>
2023-12-29 16:44:37 +0300

    Update 09_Practice.c

--- CSCP_Practice/09_Practice.c
+++ CSCP_Practice/09_Practice.c
@@ -5,7 +5,7 @@ int main(void)
 	int x = 0;
 	int y = 1;
 
-	printf("%d\n", !!x == !!y > x + 1);      //1
-	printf("%d\n", !!y > !y == !!x < !x);    //1
-	printf("%d\n", !x + !!x + y - !y - !!y); //1
+	printf("%d\n", !!x == !!y > x + 1);     
+	printf("%d\n", !!y > !y == !!x < !x);   
+	printf("%d\n", !x + !!x + y - !y - !!y);
 }
